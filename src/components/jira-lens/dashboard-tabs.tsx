a6e5cd6faeb0a45a6024de4390247a247c5bef05
Armen <armentest87@gmail.com>
2025-08-01 12:52:20 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/jira-lens/dashboard-tabs.tsx
+++ src/components/jira-lens/dashboard-tabs.tsx
@@ -1,22 +1,12 @@
 'use client';
 import { type JiraIssue } from "@/lib/types";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Overview } from "./overview";
-import { SprintAnalysis } from "./sprint-analysis";
-import { TimeworkReport } from "./timework-report";
 
 
 export function DashboardTabs({ issues }: { issues: JiraIssue[] }) {
     return (
-        <Tabs defaultValue="overview" className="w-full">
-             <TabsList className="grid w-full grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mb-4">
-                <TabsTrigger value="overview">Overview</TabsTrigger>
-                <TabsTrigger value="sprint">Sprint Analysis</TabsTrigger>
-                <TabsTrigger value="timework">Timework Report</TabsTrigger>
-            </TabsList>
-            <TabsContent value="overview"><Overview issues={issues} /></TabsContent>
-            <TabsContent value="sprint"><SprintAnalysis issues={issues} /></TabsContent>
-            <TabsContent value="timework"><TimeworkReport issues={issues} /></TabsContent>
-        </Tabs>
+        <div className="w-full">
+           <Overview issues={issues} />
+        </div>
     );
 }
