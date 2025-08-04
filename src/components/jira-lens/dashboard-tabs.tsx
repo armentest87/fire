604bec6e31160619bfa8be38eefd224c6629ed54
Armen <armentest87@gmail.com>
2025-08-04 09:48:02 +0000

    no I mean vertical not horizontal I apologize

--- src/components/jira-lens/dashboard-tabs.tsx
+++ src/components/jira-lens/dashboard-tabs.tsx
@@ -23,8 +23,8 @@ export function DashboardTabs({ issues }: { issues: JiraIssue[] }) {
     }
     
     return (
-        <Tabs defaultValue="overview" className="w-full">
-            <TabsList className="overflow-x-auto whitespace-nowrap">
+        <Tabs defaultValue="overview" orientation="vertical" className="flex w-full space-x-4">
+            <TabsList className="flex flex-col h-full space-y-2">
                 <TabsTrigger value="overview">Overview</TabsTrigger>
                 <TabsTrigger value="cumulative-flow">Cumulative Flow</TabsTrigger>
                 <TabsTrigger value="sprint-analysis">Sprint Analysis</TabsTrigger>
@@ -33,25 +33,25 @@ export function DashboardTabs({ issues }: { issues: JiraIssue[] }) {
                 <TabsTrigger value="service-management">Service Management</TabsTrigger>
                 <TabsTrigger value="itsm-report">ITSM Report</TabsTrigger>
             </TabsList>
-            <TabsContent value="overview">
+            <TabsContent value="overview" className="flex-1">
                  <Overview issues={issues} />
             </TabsContent>
-            <TabsContent value="cumulative-flow">
+            <TabsContent value="cumulative-flow" className="flex-1">
                 <CumulativeFlowDiagram issues={issues} />
             </TabsContent>
-            <TabsContent value="sprint-analysis">
+            <TabsContent value="sprint-analysis" className="flex-1">
                 <SprintAnalysis issues={issues} />
             </TabsContent>
-             <TabsContent value="sprint-time-report">
+             <TabsContent value="sprint-time-report" className="flex-1">
                 <SprintTimeReport issues={issues} />
             </TabsContent>
-            <TabsContent value="timework-report">
+            <TabsContent value="timework-report" className="flex-1">
                 <TimeworkReport issues={issues} />
             </TabsContent>
-            <TabsContent value="service-management">
+            <TabsContent value="service-management" className="flex-1">
                 <ServiceManagement issues={issues} />
             </TabsContent>
-            <TabsContent value="itsm-report">
+            <TabsContent value="itsm-report" className="flex-1">
                 <ItsmIssuesCreatedReport issues={issues} />
             </TabsContent>
         </Tabs>
